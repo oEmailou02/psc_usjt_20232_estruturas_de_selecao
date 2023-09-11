1f77050f4345eb7f86dae810afc4782fbe2eb52f
Emilio Gabriel <emiliogabrielsantos123@gmail.com>
2023-09-11 20:53:16 -0300

    ap03(selecao): import e estrutura

---
+++ exemploIf.java
@@ -0,0 +1,13 @@
+//java.lang é o pacote padrão do java
+import javax.swing.JOptionPane;
+public class exemploIf {
+    static public void main(String [] args){
+        //declaração de variáveis
+
+        //entrada
+
+        //processamento
+
+        //saída
+    }
+}
